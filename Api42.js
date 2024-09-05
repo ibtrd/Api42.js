404e9a4876597a72b5c7f54090b7d34f41047a35
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-09-05 19:25:07 +0200

    Add titles related methods

--- Api42.js
+++ Api42.js
@@ -6,88 +6,90 @@ const { tokenOptions } = require('./utils/tokenOpions');
 module.exports.Api42 = class Api42 {
   #token;
   #expiration;
-  #throttle;
+  #throttle = throttledQueue(2, 1100, true);
+  #site = "https://api.intra.42.fr";
 
   secretValidUntil;
-
-  constructor() {
-    this.#throttle = throttledQueue(2, 1050, true);
-  }
 
   async #getToken() {
     if (this.#token && Date.now() < this.#expiration) return;
 
-    await fetch("https://api.intra.42.fr/oauth/token", tokenOptions)
-      .then((response) => {
+    await fetch(`${this.#site}/oauth/token`, tokenOptions)
+      .then(async (response) => {
         if (response.ok) return response.json();
-        throw new Error("Failed to generate 42Api token", response.json());
+        if (process.env.API42_DEV) console.error(await response.json());
+        throw new Error("Failed to generate 42Api token");
       })
       .then((responseJson) => {
         this.#token = responseJson.access_token;
-        this.#expiration = (responseJson.expires_in * 1000) + Date.now();
+        this.#expiration = responseJson.expires_in * 1000 + Date.now();
         this.secretValidUntil = responseJson.secret_valid_until;
         console.warn("42API token generated.");
-      })
+      });
   }
 
   async #fetchUrl(endpoint, pagination) {
-    try { await this.#getToken();} catch {return ;}
-    
-    if (process.env.API42_DEV)
-      console.warn(`${endpoint}`);
+    try { await this.#getToken(); } catch { return; }
+    if (process.env.API42_DEV) console.warn(`${endpoint}`);
     return this.#throttle(() => {
       const responseJson = fetch(`${endpoint}`, {
         method: "GET",
         headers: {
           Authorization: `Bearer ${this.#token}`,
         },
-      }).then((response) => {
+      }).then(async (response) => {
         if (!response.ok) {
-          throw new Error({endpoint: endpoint, response: response.json()});
+          if (process.env.API42_DEV) {
+            console.error("Error:", {
+              status: response.status,
+              statusText: response.statusText,
+            });
+          }
+          throw new Error(
+            `${response.status} ${response.statusText} - ${endpoint}`
+          );
         }
         if (pagination) {
           var links = parse(response.headers.get("link"));
           return {
-            next: (links && links.next) ? links.next.url : null,
-            responseJson: response.json()
+            next: links && links.next ? links.next.url : null,
+            responseJson: response.json(),
           };
         } else {
           return response.json();
         }
-      })
-      return responseJson
-    ;})
+      });
+      return responseJson;
+    });
   }
 
   async #paginatedFetch(endpoint, perPage) {
-    if (!perPage)
-      perPage = 100;
+    if (!perPage) perPage = 100;
     endpoint.indexOf("?") > 1 ? (endpoint += "&") : (endpoint += "?");
     endpoint += `per_page=${perPage}`;
-    
+
     var responsesJson = [];
     do {
       var response = await this.#fetchUrl(endpoint, 1);
       responsesJson.push(...(await response.responseJson));
       endpoint = response.next;
     } while (response.next);
-    return (responsesJson);
+    return responsesJson;
   }
 
   async getUser(user) {
-    return this.#fetchUrl(`https://api.intra.42.fr/v2/users/${user}`);
+    return this.#fetchUrl(`${this.#site}/v2/users/${user}`);
   }
 
   async getCampusLocations(campus, active) {
-    let url = `https://api.intra.42.fr/v2/campus/${campus}/locations`;
-    if (active !== undefined)
-      url += `?filter[active]=${active}`;
-    url += '&sort=host';
+    let url = `${this.#site}/v2/campus/${campus}/locations`;
+    if (active !== undefined) url += `?filter[active]=${active}`;
+    url += "&sort=host";
     return await this.#paginatedFetch(url);
   }
 
   async getPisciners(campus, year, month) {
-    let url = `https://api.intra.42.fr/v2/campus/${campus}/users`;
+    let url = `${this.#site}/v2/campus/${campus}/users`;
     if (year && month) {
       url += `?filter[pool_year]=${year}&filter[pool_month]=${month}`;
     } else if (year) {
@@ -97,7 +99,7 @@ module.exports.Api42 = class Api42 {
   }
 
   async getUserLogtime(userID, begin, end) {
-    let url = `https://api.intra.42.fr/v2/users/${userID}/locations_stats`;
+    let url = `${this.#site}/v2/users/${userID}/locations_stats`;
     if (begin & end) {
       url += `?begin_at=${begin}&end_at=${end}`;
     }
@@ -109,26 +111,39 @@ module.exports.Api42 = class Api42 {
   }
 
   async getCampusUsuers(campus) {
-    return this.#fetchUrl(`https://api.intra.42.fr/v2/campus/${campus}/users`);
+    return this.#fetchUrl(`${this.#site}/v2/campus/${campus}/users`);
   }
 
   async getCursusProjects(cursus) {
-    return await this.#paginatedFetch(
-      `https://api.intra.42.fr/v2/cursus/${cursus}/projects`
-    );
+    return await this.#paginatedFetch(`${this.#site}/v2/cursus/${cursus}/projects`);
   }
 
   async getUserProjects(user) {
-    return await this.#paginatedFetch(
-      `https://api.intra.42.fr/v2/users/${user}/projects_users`
-    );
-  }
-
-  async getAllCursus() {
-    return await this.#paginatedFetch("https://api.intra.42.fr/v2/cursus");
+    return await this.#paginatedFetch(`${this.#site}/v2/users/${user}/projects_users`);
   }
 
   async getEventUsers(eventId) {
-    return await this.#paginatedFetch(`https://api.intra.42.fr/v2/events/${eventId}/events_users`);
+    return await this.#paginatedFetch(`${this.#site}/v2/events/${eventId}/events_users`);
   }
+
+  async getTitleUsers(titleId) {
+    return await this.#paginatedFetch(`${this.#site}/v2/titles/${titleId}/titles_users`);
+  }
+
+  async getGroupUsers(groupId) {
+    return await this.#paginatedFetch(`${this.#site}/v2/groups/${groupId}/groups_users`);
+  }
+  
+  async getAllCursus() {
+    return await this.#paginatedFetch(`${this.#site}/v2/cursus`);
+  }
+
+  async getAllGroups() {
+    return await this.#paginatedFetch(`${this.#site}/v2/groups`);
+  }
+
+  async getAllTitles() {
+    return await this.#paginatedFetch(`${this.#site}/v2/titles`);
+  }
+
 };
